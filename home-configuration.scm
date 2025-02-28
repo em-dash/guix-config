10c944b3ff02f95f64b7ecfcad0caf24644fdc0b
Meredith Oleander <33614480+em-dash@users.noreply.github.com>
2025-03-01 07:29:54 +1100

    less pain

--- home-configuration.scm
+++ home-configuration.scm
@@ -14,70 +14,82 @@
              (guix channels))
 
 (home-environment
-  (packages (map specification->package
-                 '("gcc-toolchain" "labwc"
-                   "ghostty"
-                   "firefox"
-                   "xset"
-                   "brightnessctl"
-                   "emacs"
-                   "emacs-meow"
-                   "emacs-which-key"
-                   "emacs-undo-tree"
-                   "emacs-doom-themes"
-                   "guile-lsp-server"
-                   "kanshi"
-                   "waybar@0.11.0"
-                   "mako"
-                   "swayidle"
-                   "swaybg"
-                   "kitty"
-                   "helix@23.10"
-                   "git"
-                   "openssh"
-                   "rofi"
-                   "font-google-noto"
-                   "font-google-noto-emoji"
-                   "font-google-noto-sans-cjk"
-                   "font-recursive"
-                   "zig"
-                   "acpi"
-                   "bat"
-                   "cloc"
-                   "gcc-toolchain"
-                   "grim"
-                   "slurp")))
+ (packages (map specification->package
+                '("gcc-toolchain"
+                  "labwc"
+                  "ghostty"
+                  "firefox"
+                  "xset"
+                  "brightnessctl"
+                  "emacs"
+                  "emacs-meow"
+                  "emacs-which-key"
+                  "emacs-undo-tree"
+                  "emacs-doom-themes"
+                  "guile-lsp-server"
+                  "kanshi"
+                  "waybar@0.11.0"
+                  "mako"
+                  "swayidle"
+                  "swaybg"
+                  "kitty"
+                  "helix@23.10"
+                  "git"
+                  "openssh"
+                  "rofi"
+                  "font-google-noto"
+                  "font-google-noto-emoji"
+                  "font-google-noto-sans-cjk"
+                  "font-recursive"
+                  "zig"
+                  "acpi"
+                  "bat"
+                  "cloc"
+                  "gcc-toolchain"
+                  "grim"
+                  "slurp")))
 
-  (services
-   (append (list (service home-fish-service-type
-                          (home-fish-configuration (environment-variables '(("GITHUBMAIL" . "33614480+em-dash@users.noreply.github.com")))))
-                 (abbreviations '(("la" . "ls -A") ("ll" . "ls -lAh")
-                                  ("lsl" . "ls -lAh | bat --style=plain")
-                                  ("tg" . "grep -rI --exclude-dir .* --exclude .* . -e")))))
+ (services
+  (append
+   (list
+    (service home-fish-service-type
+             (home-fish-configuration
+              (environment-variables
+               '(("GITHUBMAIL" . "33614480+em-dash@users.noreply.github.com")))
+              (abbreviations
+               '(("la" . "ls -A")
+                 ("ll" . "ls -lAh")
+                 ("lsl" . "ls -lAh | bat --style=plain")
+                 ("tg" . "grep -rI --exclude-dir .* --exclude .* . -e")))
+              (aliases '(("quit" . "exit")))))
 
-   (service home-xdg-configuration-files-service-type
-            `( ;helix
-               ("helix/config.toml" ,(local-file "./helix-config.toml"))
-              ("helix/languages.toml" ,(local-file "./helix-languages.toml"))
-              ;; labwc
-              ("labwc/autostart" ,(local-file "labwc/autostart"))
-              ("labwc/environment" ,(local-file "labwc/environment"))
-              ("labwc/menu.xml" ,(local-file "labwc/menu.xml"))
-              ("labwc/rc.xml" ,(local-file "labwc/rc.xml"))
-              ("labwc/shutdown" ,(local-file "labwc/shutdown"))
-              ("labwc/themerc" ,(local-file "labwc/themerc"))
-              ;; ghostty
-              ("ghostty/config" ,(local-file "ghostty-config"))
-              ;; rofi
-              ("rofi/config.rasi" ,(local-file "rofi-config.rasi"))))
-   (service home-channels-service-type
-            (append (list (channel
+    (service home-xdg-configuration-files-service-type
+             `( ;helix
+               ("helix/config.toml" ,(local-file
+                                      "./helix-config.toml"))
+               ("helix/languages.toml" ,(local-file
+                                         "./helix-languages.toml"))
+               ;; labwc
+               ("labwc/autostart" ,(local-file "labwc/autostart"))
+               ("labwc/environment" ,(local-file
+                                      "labwc/environment"))
+               ("labwc/menu.xml" ,(local-file "labwc/menu.xml"))
+               ("labwc/rc.xml" ,(local-file "labwc/rc.xml"))
+               ("labwc/shutdown" ,(local-file "labwc/shutdown"))
+               ("labwc/themerc" ,(local-file "labwc/themerc"))
+               ;; ghostty
+               ("ghostty/config" ,(local-file "ghostty-config"))
+               ;; rofi
+               ("rofi/config.rasi" ,(local-file
+                                     "rofi-config.rasi"))))
+    (service home-channels-service-type
+             (append (list (channel
                             (name 'saayix)
-                            (url "https://codeberg.org/look/saayix")
+                            (url
+                             "https://codeberg.org/look/saayix")
                             (branch "entropy"))
-                          (channel
+                           (channel
                             (name 'nonguix)
-                            (url "https://gitlab.com/nonguix/nonguix")))
-                    %default-channels))) %base-home-services)
-unbalanced
-unbalanced
+                            (url
+                             "https://gitlab.com/nonguix/nonguix")))
+                     %default-channels))) %base-home-services)))
