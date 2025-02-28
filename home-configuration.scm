bdd8f3e73a00debeb1dd57b5220d42d5870fdf8d
Meredith Oleander <33614480+em-dash@users.noreply.github.com>
2025-03-01 07:18:41 +1100

    pain

--- home-configuration.scm
+++ home-configuration.scm
@@ -15,17 +15,19 @@
 
 (home-environment
   (packages (map specification->package
-                 '("labwc" "rofi"
+                 '("gcc-toolchain" "labwc"
                    "ghostty"
+                   "firefox"
                    "xset"
                    "brightnessctl"
                    "emacs"
                    "emacs-meow"
                    "emacs-which-key"
-                   "emacs-paredit"
+                   "emacs-undo-tree"
+                   "emacs-doom-themes"
                    "guile-lsp-server"
                    "kanshi"
-                   "waybar"
+                   "waybar@0.11.0"
                    "mako"
                    "swayidle"
                    "swaybg"
@@ -33,66 +35,49 @@
                    "helix@23.10"
                    "git"
                    "openssh"
+                   "rofi"
                    "font-google-noto"
                    "font-google-noto-emoji"
                    "font-google-noto-sans-cjk"
+                   "font-recursive"
                    "zig"
                    "acpi"
                    "bat"
                    "cloc"
                    "gcc-toolchain"
-                   "ungoogled-chromium-wayland"
                    "grim"
                    "slurp")))
 
   (services
    (append (list (service home-fish-service-type
-                          (home-fish-configuration (aliases '(("ls"
-                                                               "ls --color=auto")
-                                                              ("la" "ls -A")
-                                                              ("ll" "ls -lAh")
-                                                              ("lsl"
-                                                               "ls -lAh | less")
-                                                              ("asdf"
-                                                               "setxkbmap us colemak")
-                                                              ("quit" exit)
-                                                              ("grep"
-                                                               "grep --color=auto")
-                                                              ("doasedit"
-                                                               "doas helix --config ~/.config/helix/config.toml")
-                                                              ("tg"
-                                                               "grep -rI --exclude-dir .* --exclude .* . -e")))
-                                                   (environment-variables '(("GITHUBMAIL"
-                                                                             "33614480+em-dash@users.noreply.github.com")))
+                          (home-fish-configuration (environment-variables '(("GITHUBMAIL" . "33614480+em-dash@users.noreply.github.com")))))
+                 (abbreviations '(("la" . "ls -A") ("ll" . "ls -lAh")
+                                  ("lsl" . "ls -lAh | bat --style=plain")
+                                  ("tg" . "grep -rI --exclude-dir .* --exclude .* . -e")))))
 
-                                                   ))
-                 (service home-xdg-configuration-files-service-type
-                          `( ;helix
-                             ("helix/config.toml" ,(local-file
-                                                    "./helix-config.toml"))
-                            ("helix/languages.toml" ,(local-file
-                                                      "./helix-languages.toml"))
-                            ;; labwc
-                            ("labwc/autostart" ,(local-file "labwc/autostart"))
-                            ("labwc/environment" ,(local-file
-                                                   "labwc/environment"))
-                            ("labwc/menu.xml" ,(local-file "labwc/menu.xml"))
-                            ("labwc/rc.xml" ,(local-file "labwc/rc.xml"))
-                            ("labwc/shutdown" ,(local-file "labwc/shutdown"))
-                            ("labwc/themerc" ,(local-file "labwc/themerc"))
-                            ;; ghostty
-                            ("ghostty/config" ,(local-file "ghostty-config"))
-                            ;; rofi
-                            ("rofi/config.rasi" ,(local-file
-                                                  "rofi-config.rasi"))))
-                 (service home-channels-service-type
-                          (append (list (channel
-                                          (name 'saayix)
-                                          (url
-                                           "https://codeberg.org/look/saayix")
-                                          (branch "entropy"))
-                                        (channel
-                                          (name 'nonguix)
-                                          (url
-                                           "https://gitlab.com/nonguix/nonguix")))
-                                  %default-channels))) %base-home-services)))
+   (service home-xdg-configuration-files-service-type
+            `( ;helix
+               ("helix/config.toml" ,(local-file "./helix-config.toml"))
+              ("helix/languages.toml" ,(local-file "./helix-languages.toml"))
+              ;; labwc
+              ("labwc/autostart" ,(local-file "labwc/autostart"))
+              ("labwc/environment" ,(local-file "labwc/environment"))
+              ("labwc/menu.xml" ,(local-file "labwc/menu.xml"))
+              ("labwc/rc.xml" ,(local-file "labwc/rc.xml"))
+              ("labwc/shutdown" ,(local-file "labwc/shutdown"))
+              ("labwc/themerc" ,(local-file "labwc/themerc"))
+              ;; ghostty
+              ("ghostty/config" ,(local-file "ghostty-config"))
+              ;; rofi
+              ("rofi/config.rasi" ,(local-file "rofi-config.rasi"))))
+   (service home-channels-service-type
+            (append (list (channel
+                            (name 'saayix)
+                            (url "https://codeberg.org/look/saayix")
+                            (branch "entropy"))
+                          (channel
+                            (name 'nonguix)
+                            (url "https://gitlab.com/nonguix/nonguix")))
+                    %default-channels))) %base-home-services)
+unbalanced
+unbalanced
